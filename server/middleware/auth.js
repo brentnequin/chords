f879a12f131029269650ea88dcb9fa85ea0b5f6c
Brent Nequin <brentnequin@gmail.com>
2023-03-21 00:03:44 -0500

    auth only for api routes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,8 @@
 export default defineEventHandler((event) => {
-    if (event.req.headers['x-api-key'] !== process.env.API_KEY) {
-        setResponseStatus(event, 401)
-        return { message: 'Unauthorized' }
+    if (event.path.startsWith('/api')) {
+        if (event.req.headers['x-api-key'] !== process.env.API_KEY) {
+            setResponseStatus(event, 401)
+            return { message: 'Unauthorized' }
+        }
     }
 })
