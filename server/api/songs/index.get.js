b6eb36ea210919e85ed1cbbd649c481e0450c7d0
Brent Nequin <brentnequin@gmail.com>
2023-03-20 23:30:56 -0500

    remove commented code

--- server/api/songs/index.get.js
+++ server/api/songs/index.get.js
@@ -1,10 +1,5 @@
 import prisma from "~~/server/data/prisma"
 
-// app.get('/api/songs', validateApiKey, async (req, res) => {
-//     const songs = await prisma.song.findMany()
-//     res.json(songs)
-// });
-
 export default defineEventHandler(async (event) => {
     const songs = await prisma.song.findMany()
     return songs
