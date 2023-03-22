da47f65999631a71faadaf9029f09e7a8fd8a01c
Brent Nequin <brentnequin@gmail.com>
2023-03-21 21:14:59 -0500

    font mono, spacing

--- pages/edit/[id].vue
+++ pages/edit/[id].vue
@@ -16,10 +16,12 @@
                 </div>
             </div>
             <div class="col-span-3 space-y-4">
-                <textarea class="p-8 w-full rounded-lg bg-slate-800" v-model="content"></textarea>
+                <textarea class="p-8 w-full rounded-lg bg-slate-800 font-mono" v-model="content"></textarea>
                 <Chords name="Preview" :content="content" />
-                <button @click="saveSong" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
-                <button @click="goToSong" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</button>
+                <div class="space-x-4">
+                    <button @click="saveSong" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
+                    <button @click="goToSong" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</button>
+                </div>
             </div>
         </div>
     </form>
