748515acddcd241f1b07350e9efacea9f8fe46f4
Brent Nequin <brentnequin@gmail.com>
2023-03-27 19:39:18 -0500

    dropdown

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="flex items-center justify-between m-8">
+    <nav class="flex items-center justify-between m-8">
         <h1 class="text-4xl"><a href="/">Chords</a></h1>
-        <div class="flex items-center space-x-8">
-            <button v-if="status === 'authenticated'" @click="signOut">Logout</button>
-            <button v-else @click="signIn('auth0')">Login</button>
-            <p v-if="status === 'authenticated'"><a href="#">{{ data.user.name }}</a></p>
+        <div class="invisible tablet:visible desktop:visible flex items-center space-x-8">
+            <button v-if="status === 'unauthenticated'" @click="signIn('auth0')">Login</button>
+            <Dropdown v-if="status === 'authenticated'" :text="data.user.name">
+                <p><a href="#">Profile</a></p>
+                <button v-if="status === 'authenticated'" @click="signOut">Logout</button>
+            </Dropdown>
             <SearchBar />
             <p><a href="/browse">Browse</a></p>
             <p><a href="/upload">Upload</a></p>
         </div>
-    </div>
+    </nav>
 </template>
 
 <script setup>
---
+++ components/Dropdown.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="relative">
+        <button class="flex items-center space-x-1" @click="toggle">
+            <p>{{ text }}</p>
+            <ChevronUp v-if="isVisible" />
+            <ChevronDown v-else />
+        </button>
+        <div v-if="isVisible" class="z-10 bg-slate-700 rounded-lg absolute mt-4 space-y-2 px-4 py-2 min-w-full">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import ChevronUp from '~icons/mdi/chevron-up'
+import ChevronDown from '~icons/mdi/chevron-down'
+
+const props = defineProps({
+    text: String
+})
+
+const isVisible = ref(false)
+const toggle = () => isVisible.value = !isVisible.value
+</script>
